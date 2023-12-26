aaab00dbeb3851a18b2d0c80174f3656ac81647b
Haric0tmer <laurent.boualavong@hotmail.fr>
2023-12-26 14:36:47 +0100

    section CTA

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,8 +1,14 @@
 import './cta.css';
 function Cta() {
     return (
-        <div>
-        <h1>Cta</h1>
+        <div className='gpt3__cta'>
+            <div className='gpt3__cta-content'>
+                <p>Request early access</p>
+                <h3>Register today & start exploring the endless possiblities.</h3>
+            </div>
+            <div className='gpt3__cta-button'>
+                <button type="button">Get Started</button>
+            </div>
         </div>
     );
 }
